39b1e0abf4109815aff388f6d0f20a0132ddde6c
ap4594-max <ap4594@srmist.edu.in>
2026-03-16 14:10:39 +0530

    feature/UC1

---
+++ src/HotelBookingApp.java
@@ -0,0 +1,6 @@
+public class HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.print("Welcome to the Hotel Booking Management System.\nSystem initalized successfully");
+
+    }
+}
